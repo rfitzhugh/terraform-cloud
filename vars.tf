8df6b30e5c5a9b027852d975757e6e8bc81f22bc
Rebecca <rmfitzhugh@gmail.com>
2020-06-02 22:32:45 -0700

    init

---
+++ vars.tf
@@ -0,0 +1,47 @@
+## =============================================================================
+#  Variables - Authentication                                                  #
+## =============================================================================
+variable "aws_access_key" {
+    type        = string
+    description = "Access key authorized for this action"
+}
+
+variable "aws_secret_key" {
+    type        = string
+    description = "Secret key authorized for this action"
+}
+
+## =============================================================================
+#  Variables - Location                                                        #
+## =============================================================================
+variable "aws_region" {
+    type        = string
+    description = "Region to create S3 bucket"
+    default = "us-west-1"
+}
+
+## =============================================================================
+#  Variables - Location                                                        #
+## =============================================================================
+variable "aws_vpc" {
+    type        = string
+    description = "VPC to connect Security Group"
+}
+
+variable "aws_vpc_cidr" {
+    type        = string
+    description = "CIDR for Security Group"
+}
+
+## =============================================================================
+#  Variables - Tags                                                            #
+## =============================================================================
+variable "aws_environment_name" {
+    type        = string
+    description = "Tag for Environment"
+}
+
+variable "aws_rubrik_cdm" {
+    type        = string
+    description = "Tag for affiliated Rubrik cluster"
+}
